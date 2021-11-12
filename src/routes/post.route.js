14fe1eba338a5d25c1dcb3b2c6b4b03f78ed2112
TranBang2000 <tranthobang20001205@gmail.com>
2021-11-12 21:04:19 +0700

    final back-end

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -3,36 +3,90 @@ import PostModel from "../models/post.models";
 const verifyToken = require("../middlewares/auth.middleware");
 const routerV2 = express.Router();
 //get
-routerV2.get("/",verifyToken, (req, res) => {
-
-  //id cua user tao request 
-  PostModel.find({user:req._id})
+routerV2.get("/", verifyToken, (req, res) => {
+  //id cua user tao request
+  PostModel.find({ user: req._id }).populate("user",['username'])
     .then((data) => {
       res.json({
         success: true,
-        data:data
+        data: data,
       });
     })
     .catch((err) => res.json(err));
 });
 //post api/posts
-routerV2.post('/',verifyToken,(req,res)=>{
+routerV2.post("/", verifyToken, (req, res) => {
   const { title, description, url, status } = req.body;
-  if(!title||!description){
+  if (!title || !description) {
     return res.status(400).json({
-            success: false,
-            message: `Bạn cần nhập title và description`,
+      success: false,
+      message: `Bạn cần nhập title và description`,
     });
   }
-  PostModel.create({title,description,url,status: status || "Pending",user:req._id})
-  .then(data=>{
+  PostModel.create({
+    title,
+    description,
+    url,
+    status: status || "Pending",
+    user: req._id,
+  })
+    .then((data) => {
       res.json({
         success: true,
         message: `Hãy bắt đầu ngay thôi nào!!`,
         data: data,
       });
-  })
-  .catch((err) => res.status(500).json(`Có lỗi xảy ra:` + err));
-})
+    })
+    .catch((err) => res.status(500).json(`Có lỗi xảy ra:` + err));
+});
 
+//put
+routerV2.put("/:id", verifyToken, (req, res) => {
+  //id cua user tao request
+  const { title, description, url, status } = req.body;
+  if (!title) {
+    return res.status(400).json({
+      success: false,
+      message: `Title không được để trống`,
+    });
+  }
+  PostModel.findByIdAndUpdate({_id:req.params.id,use:req._id},{
+    title,
+    description: description||"",
+    url,
+    status: status || "Pending"
+  },{new:true})
+    .then((data) => {
+      if(data){
+        res.json({
+          success: true,
+          message: `Cập nhật thành công!!`,
+          data: data,
+        });
+      }else return res.json({
+        sucess:false,
+        message:`Cập nhật thất bại`
+      })
+    })
+    .catch((err) => res.status(500).json(`Có lỗi xảy ra:` + err));
+});
+
+//delete
+routerV2.delete("/:id", verifyToken, (req, res) => {
+  //id cua user tao request
+  PostModel.findOneAndDelete({ _id:req.params.id,user: req._id })
+  .then((data) => {
+    if(data){
+      res.json({
+        success: true,
+        message: `Xóa thành công!!`,
+        data: data,
+      });
+    }else return res.json({
+      sucess:false,
+      message:`Xóa thất bại`
+    })
+  })
+    .catch((err) => res.json(err));
+});
 module.exports = routerV2;
